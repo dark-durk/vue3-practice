b2bbc60e810f912774bd1e097ce9b3c000eccd59
宋婷 <210009804@qq.com>
2022-04-22 18:37:29 +0800

    first: commit

---
+++ .prettierrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  useTabs: false,
+  jsxSingleQuote: false,
+  arrowParens: 'avoid',
+  rangeStart: 0,
+  trailingComma: 'all',
+  endOfLine: 'crlf',
+  'Bracket Line': true,
+  /* 每种语言默认的格式化规则 */
+  '[html]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode',
+  },
+  '[css]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode',
+  },
+  '[scss]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode',
+  },
+
+  '[javascript]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode',
+  },
+
+  '[vue]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode',
+  },
+
+  '[json]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode',
+  },
+}
